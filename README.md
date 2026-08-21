bd8222e33d964a7379c90a6a2dd47dcbf8d463de
Wilmer Rondan Leon <paolorondantrujillo@gmail.com>
2026-08-20 19:12:39 -0500

    fix: agregue texto faltante

--- README.md
+++ README.md
@@ -8,3 +8,5 @@ Que esperás para ser un **gran progremador**
 
 Para poder empezar con el primer ejercicio corre el siguiente comando:
 `py empecemos.py`
+
+Muchas gracias con todos!
---
+++ semana01/01-hola-mundo.py
@@ -0,0 +1,7 @@
+saludo = "Bienvenido al curso de Backend"
+edad = 19
+
+# Los comentarios sirven para notificar a los demas como funciona el codigo
+
+# Imprimir terminal
+print("Holas!!")   
